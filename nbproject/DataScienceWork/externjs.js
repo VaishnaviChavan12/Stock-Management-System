62e45fef39fdc7d0b84377f3e5e9c6b8f460c9bc
VaishnaviChavan12 <vaishnavi.sc.2001@gmail.com>
2022-07-31 15:02:12 +0530

    Square file

--- nbproject/DataScienceWork/externjs.js
+++ nbproject/DataScienceWork/externjs.js
@@ -3,16 +3,7 @@ function getopera()
     {
       var x=document.getElementById("demo");
       n=prompt("Enter a no:");
-     
-    if(n %2 == 0)
-    {
-        alert(n+" is even");
-        x.innerHTML =n+ "is even";
-    }
-    else
-    {
-      alert(n+" is odd");
-      x.innerHTML = n+ "is odd";
-    }
+     p=n*n;
+        x.innerHTML =p+ " is square of "+n;
    
     }
